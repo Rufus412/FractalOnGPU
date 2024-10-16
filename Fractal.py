e9a52d012a69a0952add5901bb1b1e255ace4ec3
jacob <jacob@sannes.se>
2024-10-16 20:20:38 +0200

    Way faster version

--- Fractal.py
+++ Fractal.py
@@ -8,65 +8,74 @@ import math
 import time
 from tkinter import *
 from PIL import Image, ImageTk
+import unittest
 
 
 
 
 
 @cuda.jit
-def computeImage(A, B):
+def computeImage(A, B, C):
     row, col = cuda.grid(2)
-    if row < A.shape[0] and col < A.shape[1]:
-        z = A[row, col, 0] + A[row, col, 1] * 1j
+    if row < A.shape[0] and col < B.shape[1]:
+        z = A[row, col] + B[row, col] * 1j
         c = z
         iterations = 0
         while iterations < 256:
             z = z * z + c
             if abs(z) > 2:
-                B[row, col, 0] = iterations
-                B[row, col, 1] = iterations  
-                B[row, col, 2] = iterations
+                combined = (iterations + 15 << 16) | (iterations << 8) | 2 * iterations + 10 
+                C[row, col] = combined
                 break
-            elif iterations == 255:
-                B[row, col, 0] = 255
-                B[row, col, 1] = 255
-                B[row, col, 2] = 255
             iterations +=1
+        if iterations == 255:
+            C[row, col] = 0
         
 
 
 
 def makeImage(data):
     im = Image.new("RGB", imageSize)
-    pixel_data = data.reshape(-1, 3).astype(int).tolist()
-    x = list(map(tuple, pixel_data))
-    im.putdata(x)
+    im.putdata(data.flatten().tolist())
     showcanvas = ImageTk.PhotoImage(im)
     canvas.create_image(0, 0, anchor=NW, image=showcanvas)
     canvas.image = showcanvas  # Keep a reference to avoid garbage collection
     canvas.pack()
+    im.save('pillow_imagedraw.png')
 
 def calcPixleCoordiantes():
-    return np.array([[( xMin + (xMax - xMin)/imageSize[0] * x, yMin + (yMax - yMin)/imageSize[0] * y) for x in range(imageSize[0])] for y in range(imageSize[1])], dtype=np.float64)
+    #return cp.array([[( xMin + (xMax - xMin)/imageSize[0] * x, yMin + (yMax - yMin)/imageSize[0] * y) for x in range(imageSize[0])] for y in range(imageSize[1])], dtype=np.float64)
+    x_space = np.linspace(xMin, xMax,imageSize[0], dtype=np.float64)
+    y_space = np.linspace(yMin, yMax,imageSize[1], dtype=np.float64)
+    return np.meshgrid(x_space, y_space)
 
 def main():
     start = time.time()
-    pixleCoords = calcPixleCoordiantes()
-    imageData = cp.zeros((imageSize[0], imageSize[1], 3), dtype=np.uint8)
-    print(imageData)
+    start1 = time.time()
+    pixleCoords_x, pixleCoords_y = calcPixleCoordiantes()
+    end1 = time.time()
+    print(f"generate coords took {end1-start1} seconds")
+    #imageData = cp.zeros((imageSize[0], imageSize[1], 3), dtype=np.uint8)
+    imageData = cp.zeros((imageSize[0], imageSize[1]), dtype=np.int32)
+    #print(imageData)
     threadsperblock = (32, 32)  
-    blockspergrid_x = int(np.ceil(pixleCoords.shape[0] / threadsperblock[0]))
-    blockspergrid_y = int(np.ceil(pixleCoords.shape[1] / threadsperblock[1]))
+    blockspergrid_x = int(np.ceil(pixleCoords_x.shape[0] / threadsperblock[0]))
+    blockspergrid_y = int(np.ceil(pixleCoords_y.shape[1] / threadsperblock[1]))
     blockspergrid = (blockspergrid_x, blockspergrid_y)  
-    print(f"The kernel will be executed up to element {threadsperblock[0]*blockspergrid_x}")
-    computeImage[blockspergrid, threadsperblock](pixleCoords, imageData)
-   
-
+    #print(f"The kernel will be executed up to element {threadsperblock[0]*blockspergrid_x}")
     
-
+    start2 = time.time()
+    computeImage[blockspergrid, threadsperblock](pixleCoords_x, pixleCoords_y, imageData)
+   
+    end2 = time.time()
+    print(f"Compute fractal took {end2-start2} seconds")
+    
+    start3 = time.time()
     makeImage(imageData)
+    end3 = time.time()
+    print(f"Make Image took {end3-start3} seconds")
     end = time.time()
-    print(f"Math Done Time = {end-start}")
+    print(f"Time = {end-start}")
 
 if __name__ == '__main__':
     xMax = 1
@@ -74,11 +83,11 @@ if __name__ == '__main__':
     yMax = 1
     yMin = -1
     #cuda.detect()
-    imageSize = (1024, 1024)
+    imageSize = (4096, 4096)
     
     win = Tk()
     win.geometry("1200x1200")
-    canvas= Canvas(win, width= 5000, height= 5000)
+    canvas= Canvas(win, width= 1024, height= 1024)
     canvas.pack()
 
     main()
